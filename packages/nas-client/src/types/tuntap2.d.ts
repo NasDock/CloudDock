87e0d947eed17ea33a1f1de1bc082dee97ef7299
mmdctjj <49404397+mmdctjj@users.noreply.github.com>
2026-05-10 23:05:14 +0800

    Fix nas-client tuntap2 type resolution

---
+++ packages/nas-client/src/types/tuntap2.d.ts
@@ -0,0 +1,12 @@
+import { EventEmitter } from 'events';
+
+export class Tun extends EventEmitter {
+  name: string;
+  ipv4: string;
+  mtu: number;
+  isUp: boolean;
+
+  constructor();
+  write(data: Buffer): void;
+  release(): void;
+}
